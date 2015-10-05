a141c377d76aca8b173c5ca6e5514faab3aa84dd
gcampbell01 <george.campbell@vmsuk.com>
2015-10-05 11:19:45 +0100

    intial shit

--- TestImageGallery/settings.py
+++ TestImageGallery/settings.py
@@ -81,3 +81,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/home/username/dbe/media/'
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
