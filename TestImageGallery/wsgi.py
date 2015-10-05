beb934854cf6f6bcd45677aad3cfb2e170bf761e
gcampbell01 <george.campbell@vmsuk.com>
2015-10-05 09:17:05 +0100

    initial commit

---
+++ TestImageGallery/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for TestImageGallery project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TestImageGallery.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
